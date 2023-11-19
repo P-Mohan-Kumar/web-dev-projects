bf18767acb34732f3f0826ca3b6baae9897ea723
P MOHAN KUMAR <136331157+P-Mohan-Kumar@users.noreply.github.com>
2023-11-19 17:31:46 +0530

    Update server.js

--- Expense-Tracker/server.js
+++ Expense-Tracker/server.js
@@ -1,51 +1,41 @@
 const express = require('express');
-const router = express.Router();
-const Expense = require('../models/Expense');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const Expense = require('./models/Expense');
 
-// GET all expenses
-router.get('/', async (req, res) => {
-  try {
-    const expenses = await Expense.find();
-    res.json(expenses);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/expense-tracker', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Connection error:', err.message);
 });
 
-// POST a new expense
-router.post('/', async (req, res) => {
-  const { name, amount, date } = req.body;
 
-  if (!name || !amount || !date) {
-    return res.status(400).json({ message: "Please provide name, amount, and date for the expense." });
-  }
 
-  try {
-    const newExpense = await Expense.create({
-      name,
-      amount,
-      date,
-    });
+// API endpoints
+const expenseRoutes = require('./routes/expenseRoutes');
+app.use('/expenses', expenseRoutes);
 
-    res.status(201).json(newExpense);
-  } catch (err) {
-    console.error('Error adding expense:', err.message);
-    res.status(500).json({ message: 'Failed to add expense.' });
-  }
+// Serve the index.html file for the root path '/'
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// DELETE an expense
-router.delete('/:id', async (req, res) => {
-  try {
-    const deletedExpense = await Expense.findByIdAndDelete(req.params.id);
-    if (!deletedExpense) {
-      return res.status(404).json({ message: 'Expense not found' });
-    }
-    res.json({ message: 'Expense deleted' });
-  } catch (err) {
-    console.error('Error deleting expense:', err.message);
-    res.status(500).json({ message: 'Failed to delete expense' });
-  }
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-module.exports = router;
